659b502fbae0d6fd7ea7ec8fc5f6510aec533002
Luke Nizik <elejdor@gmail.com>
2015-05-07 21:14:51 +0200

    no message

--- gameOfFifteen/gameOfFifteen/main.cpp
+++ gameOfFifteen/gameOfFifteen/main.cpp
@@ -1,16 +1,23 @@
 #include <vector>
+#include <queue>
+#include <stack>
 
 #define U_CHAR unsigned char
 typedef U_CHAR byte;
 
 std::vector<byte[4][4]> visitedNodes;
 
-byte root[4][4] = {
+byte root = {
 	{ 0, 1, 2, 3 },
 	{ 4, 5, 6, 7 },
 	{ 8, 9, 10, 11 },
 	{ 12, 13, 14, 15 }
 };
+
+void IterativeBFS()
+{
+	std::queue<byte[4][4]> queue;
+}
 
 int main()
 {
