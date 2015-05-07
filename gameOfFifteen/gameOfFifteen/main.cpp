eed93781fe5f46bada4d864bc9f9e946c2ab569b
piechota <piechota33@gmail.com>
2015-05-07 23:20:54 +0200

    random function

--- gameOfFifteen/gameOfFifteen/main.cpp
+++ gameOfFifteen/gameOfFifteen/main.cpp
@@ -67,6 +67,65 @@ void IterativeBFS()
 	}
 }
 
+void RandomRoot(int difficultLevel)
+{
+	int x = 0,
+		y = 0;
+
+	byte lastMove = 0;
+
+	for (int i = 0; i < difficultLevel; ++i)
+	{
+		byte possibleMoves = ~0;
+		if (x == 0) possibleMoves &= ~(1 << 0);
+		if (y == 0) possibleMoves &= ~(1 << 1);
+		if (x == 3) possibleMoves &= ~(1 << 2);
+		if (y == 3) possibleMoves &= ~(1 << 3);
+		possibleMoves &= ~(1 << lastMove);
+
+		byte move = 0;
+		byte m;
+		do
+		{
+			m = rand() % 4;
+			move = possibleMoves & (1 << m);
+		} while (move == 0);
+
+		if (m == 0) lastMove = 2;
+		else if (m == 1) lastMove = 3;
+		else if (m == 2) lastMove = 0;
+		else if (m == 3) lastMove = 1;
+
+		byte tmp;
+		switch (move){
+		case (1 << 0) :
+			tmp = root.board[x - 1][y];
+			root.board[x - 1][y] = root.board[x][y];
+			root.board[x][y] = tmp;
+			--x;
+			break;
+		case (1 << 1):
+			tmp = root.board[x][y - 1];
+			root.board[x][y - 1] = root.board[x][y];
+			root.board[x][y] = tmp;
+			--y;
+			break;
+		case (1 << 2):
+			tmp = root.board[x + 1][y];
+			root.board[x + 1][y] = root.board[x][y];
+			root.board[x][y] = tmp;
+			++x;
+			break;
+		case (1 << 3):
+			tmp = root.board[x][y + 1];
+			root.board[x][y + 1] = root.board[x][y];
+			root.board[x][y] = tmp;
+			++y;
+			break;
+		}
+	}
+}
+
 int main()
 {
 	IterativeBFS();
