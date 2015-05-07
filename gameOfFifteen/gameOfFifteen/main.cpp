c6c7d1401bdfed2bdcbd38a26c0be431da720ce7
Luke Nizik <elejdor@gmail.com>
2015-05-07 20:24:35 +0200

    [+] board array

--- gameOfFifteen/gameOfFifteen/main.cpp
+++ gameOfFifteen/gameOfFifteen/main.cpp
@@ -1,3 +1,13 @@
+#define U_CHAR unsigned char
+typedef U_CHAR byte;
+
+byte board[4][4] = {
+	{ 0, 1, 2, 3 },
+	{ 4, 5, 6, 7 },
+	{ 8, 9, 10, 11 },
+	{ 12, 13, 14, 15 }
+};
+
 int main()
 {
 	return 0;
