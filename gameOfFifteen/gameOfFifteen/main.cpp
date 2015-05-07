382ff3fb23bff3e00a1db167653a9a5efdcdf6ee
Luke Nizik <elejdor@gmail.com>
2015-05-07 21:42:05 +0200

    [+] operator ==

--- gameOfFifteen/gameOfFifteen/main.cpp
+++ gameOfFifteen/gameOfFifteen/main.cpp
@@ -8,6 +8,21 @@ typedef U_CHAR byte;
 struct Node
 {
 	byte board[4][4];
+
+	bool operator ==(Node& a)
+	{
+		for (byte i = 0; i < 4; i++)
+		{
+			for (byte j = 0; j < 4; j++)
+			{
+				if (this->board[j][i] != a.board[j][i])
+				{
+					return false;
+				}
+			}
+		}
+		return true;
+	}
 };
 
 std::vector<Node> visitedNodes;
@@ -18,6 +33,14 @@ Node root = {
 	8, 9, 10, 11,
 	12, 13, 14, 15
 };
+
+Node solved = {
+	0, 1, 2, 3,
+	4, 5, 6, 7,
+	8, 9, 10, 11,
+	12, 13, 14, 15
+};
+
 void IterativeDFS()
 {
 
@@ -26,10 +49,21 @@ void IterativeBFS()
 {
 	std::queue<Node> queue;
 	queue.push(root);
+	Node* current;
 
 	while (!queue.empty())
 	{
+		current = &queue.back();
+		queue.pop();
 
+		if (*current == solved)
+		{
+			return;
+		}
+		else
+		{
+
+		}
 	}
 }
 
