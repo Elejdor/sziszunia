de7d0f8e04c875269776b5431b79ff72b2719bd2
Luke Nizik <elejdor@gmail.com>
2015-05-07 21:19:23 +0200

    no message

--- gameOfFifteen/gameOfFifteen/main.cpp
+++ gameOfFifteen/gameOfFifteen/main.cpp
@@ -7,7 +7,7 @@ typedef U_CHAR byte;
 
 std::vector<byte[4][4]> visitedNodes;
 
-byte root = {
+byte root[4][4] = {
 	{ 0, 1, 2, 3 },
 	{ 4, 5, 6, 7 },
 	{ 8, 9, 10, 11 },
@@ -16,7 +16,8 @@ byte root = {
 
 void IterativeBFS()
 {
-	std::queue<byte[4][4]> queue;
+	std::queue<byte[4][4]> bfsQueue;
+	bfsQueue.push(root);
 }
 
 int main()
